03e57219619fc2ec4c79550b0fa680f82f423947
Prem Prakash <premm98@Prems-MacBook-Pro.local>
2024-03-07 20:43:19 +0530

    Added mongo functions

---
+++ mongo.py
@@ -0,0 +1,16 @@
+'''Mongo related class and function'''
+
+from pymongo import MongoClient
+
+class MongoManager():
+    '''Mongo related things are encapsulated'''
+
+    def connect_to_mongo(self, mongo_url):
+        '''Connect to mongo function'''
+        client = MongoClient(mongo_url)
+        return client
+    
+    def disconnect_from_mongo(self, client):
+        '''Disconnect from Mongo'''
+        client.close()
+        return {'message':'Client closed'}
