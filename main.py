e4c96626d221766cfd4562a83f70423c23e92f5a
Premm98 <prakashprem.pp2@gmail.com>
2024-03-14 00:12:26 +0530

    refactored

--- main.py
+++ main.py
@@ -4,8 +4,6 @@ from os import environ
 from dotenv import load_dotenv
 from flask import Flask
 from flask_restx import Api, Resource
-import json
-from bson import ObjectId
 from query_builder.query import list_products
 from mongo import MongoManager
 
@@ -14,22 +12,21 @@ load_dotenv()
 mongo_url = environ['MONGO_URL']
 
 app = Flask(__name__)
-api = Api(app, title='Indi Moda APIs', version='1.0.0', default_label='Mongo APIs', default='Indi-Moda-Developement')
+api = Api(app, title='Indi Moda APIs', 
+        version='1.0.0',
+        default_label='Mongo APIs',
+        default='Indi-Moda-Developement')
 @api.route('/hello')
 class HelloWorld(Resource):
     '''Class Hello World'''
     
-
     def get(self):
         '''The Get function'''
         mongo_obj = MongoManager()
         client = mongo_obj.connect_to_mongo(mongo_url=mongo_url)
         data = mongo_obj.query_to_mongo(client=client,collection='product_details',query=list_products)
-        def serialize(obj):
-            if isinstance(obj, ObjectId):
-                return str(obj)
-            return obj
         # return json.dumps(data, default=serialize, indent=2)
+        del mongo_obj
         return list(data)
     
 
